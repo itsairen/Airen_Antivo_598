3eae347fd22706354c7e44c8495ee5c8d3e77889
Maruuu1101110 <airennantivo@gmail.com>
2026-04-22 11:06:31 +0800

    Final Exam

--- modelhandling/lib/main.dart
+++ modelhandling/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:modelhandling/screen/student_screen%20(2).dart';
+import 'package:modelhandling/screen/chat_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: StudentPage(),
+      home: ChatPage(username: "2"),
       debugShowCheckedModeBanner: false,
     );
   }
